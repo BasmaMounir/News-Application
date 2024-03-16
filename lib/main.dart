736283c1545864eb53649334482a3b1a24aa9459
BasmaMounir <basmamounir57@gimal.com>
2024-03-16 21:55:39 +0200

    + splash screen + empty home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/Home/home_screen.dart';
+import 'package:news_app/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,10 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: SplashScreen.routeName,
+      routes: {
+        SplashScreen.routeName: (context) => SplashScreen(),
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
       title: 'News App',
     );
     }
